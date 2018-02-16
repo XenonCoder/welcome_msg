c76b141edf8af31c44d107671478693c5534e208
XenonCoder <xenon77.dev@gmail.com>
2018-02-16 16:24:08 +0100

    update

--- init.lua
+++ init.lua
@@ -17,16 +17,16 @@ mt_welcome_server = {}
 -- SETTINGS 
 
 -- Message that appears on the form when accessing the server
-mt_welcome_server.welcome = "Message...";
+welcome_msg.welcome = "Message...";
 
-mt_welcome_server.players = {};
+welcome_msg.players = {};
 local worldpath = minetest.get_worldpath();
 
 minetest.register_on_joinplayer(function(player) 
 	local name = player:get_player_name(); if name == nil then return end
 	
 	-- read player inventory data
-	local form = "size [6,2] textarea[0,0;6.6,3.5;help;*** Form title ***;".. mt_welcome_server.welcome.."]" ..
+	local form = "size [6,2] textarea[0,0;6.6,3.5;help;*** Form title ***;".. welcome_msg.welcome.."]" ..
 	"button_exit[0,1;1,2;exit;Ok]"
 	        -- minetest.chat_send_player(name, form_msg.welcome);
 			minetest.show_formspec(name, "mt_welcome_server:welcome", form)
